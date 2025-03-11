d82ad2a7f9d7c65a555219314c177b119709dc45
Jacob Kustra <jacobkustra@gmail.com>
2025-03-10 21:49:22 -0700

    Cleaned up test code, keys pressed are stored, resulting in movement if latest key is removed, defaulting to second latest and so on.

--- snake.py
+++ snake.py
@@ -21,7 +21,7 @@ dt = 0
 player_pos = pygame.Vector2((screen.get_width() / 2), 
                             (screen.get_height() / 2))
 
-
+# Function that decides way movement will occur when key is pressed
 def keys(key):
     if key == pygame.K_UP:
         player_pos.y -= 300 * dt
@@ -32,8 +32,8 @@ def keys(key):
     if key == pygame.K_RIGHT:
         player_pos.x += 300 * dt
 
-
-test = []
+# Stores keys pressed
+keys_pressed = []
 
 # Game loop
 while running:
@@ -46,26 +46,21 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             # Instead add keys to a list, always returning last item?
-            test.append(event.key)
-            print(f"added key {event.key}")
+            keys_pressed.append(event.key)
         
         if event.type == pygame.KEYUP:
             # Instead add keys to a list, always returning last item?
-            test.remove(event.key)
-            print(f"removed key {event.key}")
-
-
+            keys_pressed.remove(event.key)
 
     # Makes screen blue and wipes away previous frame
     screen.fill("green")
-
 
     # Draws snake
     pygame.draw.circle(screen, "red", player_pos, 10)
 
     # Checks if keys are pressed and moves snake
-    if test:
-        keys(test[-1])
+    if keys_pressed:
+        keys(keys_pressed[-1])
 
     # flip() displays shows work
     pygame.display.flip()
