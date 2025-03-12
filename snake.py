8e86b8222a5e6a0f000b11e0eac3c16dc859b4c7
Jacob Kustra <jacobkustra@gmail.com>
2025-03-11 17:34:36 -0700

    Reworking to use grid style layout/movement, larger window size, and dynamic size of snake/grid cells to ensure compatability with all display sizes.

--- snake.py
+++ snake.py
@@ -4,59 +4,22 @@ import pygame
 
 # Initialize pygame
 pygame.init()
+pygame.display.init()
 
 # Set screen size
-screen_width = 1280
-screen_height = 720
+screen_width = pygame.display.Info().current_w
+screen_height = pygame.display.Info().current_h
 screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Grid set up
+# Cell size and snake/fruit size should be based off of set number of columns 
+# and rows and dynamic to window size 
 
 # Set up game clock
 clock = pygame.time.Clock()
 
 # Game loop is active
 running = True
-
-dt = 0
-
-# Width of Snake
-snake_size = 20
-
-# Player position with starting location
-player_pos = pygame.Vector2((screen_width / 2), (screen_height / 2))
-
-# Function that ends game if snake collides with wall
-def hit_wall():
-    print(player_pos.x, player_pos.y)
-    global running
-    if player_pos.x <= 0:
-        running = False
-    elif player_pos.x >= (screen_width - (snake_size - 2)):
-        running = False
-    elif player_pos.y <= 0:
-        running = False
-    elif player_pos.y >= (screen_height - (snake_size - 2)):
-        running = False
-
-# Ensures only these keys result in valid game interaction
-eligible_keys = [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]
-
-# Function that decides way movement will occur when key is pressed
-def keys(key):
-    if key == pygame.K_UP:
-        if player_pos.y >= 0:
-            player_pos.y -= 300 * dt
-    if key == pygame.K_DOWN:
-        if player_pos.y <= (screen.get_height() - (snake_size - 2)):
-            player_pos.y += 300 * dt
-    if key == pygame.K_LEFT:
-        if player_pos.x >= 0:
-            player_pos.x -= 300 * dt
-    if key == pygame.K_RIGHT:
-        if player_pos.x <= (screen.get_width() - (snake_size - 2)):
-            player_pos.x += 300 * dt
-
-# Stores keys pressed
-key_pressed = []
 
 # Game loop
 while running:
@@ -67,32 +30,13 @@ while running:
             # Ends game loop
             running = False
 
-        if event.type == pygame.KEYDOWN:
-            if event.key in eligible_keys:
-                key_pressed.clear()
-                key_pressed.append(event.key)
-        
+    # Makes screen white and wipes away previous frame
+    screen.fill("white")
 
-    # Makes screen blue and wipes away previous frame
-    screen.fill("green")
-
-    # Draws snake
-    # pygame.draw.circle(screen, "red", player_pos, 10)
-    pygame.draw.rect(screen, "red", 
-                     (player_pos.x, player_pos.y, snake_size, snake_size))
-
-    # Checks if keys are pressed and moves snake
-    if key_pressed:
-        keys(key_pressed[0])
-    
     # flip() displays shows work
     pygame.display.flip()
 
-
-    hit_wall()
-
-    # Limits the FPS to 60 and adjusts delta time 
-    dt = clock.tick(60) / 1000
+    clock.tick(60)
 
 pygame.quit()
 
